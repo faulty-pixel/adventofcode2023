adc440778c5fe393ee279d09941eff8831d6b81e
faulty-pixel <vishnuvilambath@gmail.com>
2023-12-12 00:46:48 +0530

    solves day 1 part 2

--- day1/day1.erl
+++ day1/day1.erl
@@ -1,6 +1,7 @@
 -module(day1).
 -export([
-    part1/1
+    part1/1,
+    part2/1
 ]).
 
 readfile(FileName) ->
@@ -23,3 +24,45 @@ number([H | T]) when T =/= [] ->
 number([H]) ->
     {N, _} = string:to_integer([H, H]),
     N.
+
+part2(FileName) ->
+    Lines = readfile(FileName),
+    Numbers = [filter_digits_pro(Line) || Line <- Lines],
+    print_nums(Numbers),
+    Sum = lists:sum(Numbers),
+    io:format("~p~n", [Sum]).
+
+print_nums([]) ->
+    ok;
+print_nums([H | T]) ->
+    io:format("~p~n", [H]),
+    print_nums(T).
+
+filter_digits_pro(Line) ->
+    {FNo, ok} = first_number(Line),
+    {LNo, ok} = last_number(lists:reverse(Line)),
+    FNo * 10 + LNo.
+
+first_number([X | _]) when X >= 48, X < 58 -> {X - 48, ok};
+first_number("one" ++ _) -> {1, ok};
+first_number("two" ++ _) -> {2, ok};
+first_number("three" ++ _) -> {3, ok};
+first_number("four" ++ _) -> {4, ok};
+first_number("five" ++ _) -> {5, ok};
+first_number("six" ++ _) -> {6, ok};
+first_number("seven" ++ _) -> {7, ok};
+first_number("eight" ++ _) -> {8, ok};
+first_number("nine" ++ _) -> {9, ok};
+first_number([_ | T]) -> first_number(T).
+
+last_number([X | _]) when X >= 48, X < 58 -> {X - 48, ok};
+last_number("eno" ++ _) -> {1, ok};
+last_number("owt" ++ _) -> {2, ok};
+last_number("eerht" ++ _) -> {3, ok};
+last_number("ruof" ++ _) -> {4, ok};
+last_number("evif" ++ _) -> {5, ok};
+last_number("xis" ++ _) -> {6, ok};
+last_number("neves" ++ _) -> {7, ok};
+last_number("thgie" ++ _) -> {8, ok};
+last_number("enin" ++ _) -> {9, ok};
+last_number([_ | T]) -> last_number(T).
